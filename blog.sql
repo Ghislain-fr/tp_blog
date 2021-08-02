c0a1ab7429e35b4ef7cbe31428865cf3b8529579
sebhory <barry5655@gmail.com>
2021-08-02 10:50:51 +0200

    version 1 du projet avec la db blog

--- blog.sql
+++ blog.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 29 juil. 2021 à 11:56
--- Version du serveur :  5.7.31
--- Version de PHP : 7.4.9
+-- Host: localhost:3307
+-- Generation Time: Aug 02, 2021 at 08:49 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,98 +19,204 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `blog`
+-- Database: `blog`
 --
-CREATE DATABASE blog CHARACTER SET "UTF8";
-USE blog;
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `articles`
+-- Table structure for table `articles`
 --
 
-DROP TABLE IF EXISTS `articles`;
-CREATE TABLE IF NOT EXISTS `articles` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `titre` varchar(50) NOT NULL,
-  `contenu` longtext NOT NULL,
-  `image_article` longblob NOT NULL,
+CREATE TABLE `articles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `titre` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contenu` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image_article` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `date_creation` datetime NOT NULL,
   `user_id` int(10) UNSIGNED DEFAULT NULL,
   `categorie_id` int(10) UNSIGNED DEFAULT NULL,
-  `temps_lecture` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_articles_categories` (`categorie_id`),
-  KEY `fk_articles_users` (`user_id`)
+  `temps_lecture` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `titre`, `contenu`, `image_article`, `date_creation`, `user_id`, `categorie_id`, `temps_lecture`) VALUES
+(1, 'titre 1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/cards.png', '2021-07-31 00:32:03', 1, 1, NULL),
+(2, 'titre 2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/cards.png', '2021-07-31 00:33:39', 1, 2, NULL),
+(3, 'titre 3', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/cards.png', '2021-07-31 00:34:12', 1, 3, NULL),
+(4, 'titre 4 modifier 3fois ', '   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.   ', 'img/version 1  ecootree.PNG', '2021-07-31 04:37:12', 1, 2, NULL),
+(5, 'barry2 titre 1', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/douglas_jeune_plant.jpg', '2021-07-31 00:44:50', 2, 1, NULL),
+(6, 'barry2 titre 2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/photo 2.jpg', '2021-07-31 00:47:09', 2, 2, NULL),
+(7, 'barry2 titre 3', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/gift-box.png', '2021-07-31 00:49:05', 2, 3, NULL),
+(8, 'barry2 titre 4', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'img/photo1.jpg', '2021-07-31 00:49:27', 2, 4, NULL),
+(9, 'titre 5  modifier', '  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  ', 'img/cards.png', '2021-07-31 04:25:48', 1, NULL, NULL),
+(10, 'article blouf ', '  dsfdkjfmdFLDKF  ', 'img/JCVD.jpg', '2021-08-02 09:44:34', 3, 2, NULL),
+(11, 'tatatatata  ', '  mdlfdsmfkdsùlqdsgfsklgfsq  ', 'img/interrogation.jpg', '2021-08-02 09:44:10', 3, 2, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categories`
+-- Table structure for table `categories`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` text NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `nom`) VALUES
+(1, 'Litterature'),
+(2, 'Musique'),
+(3, 'Cinema'),
+(4, 'Sport');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `commentaires`
+-- Table structure for table `commentaires`
 --
 
-DROP TABLE IF EXISTS `commentaires`;
-CREATE TABLE IF NOT EXISTS `commentaires` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) UNSIGNED DEFAULT NULL,
+CREATE TABLE `commentaires` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
   `date_creation` datetime NOT NULL,
-  `contenu` longtext NOT NULL,
-  `article_id` int(10) UNSIGNED DEFAULT NULL,
-  `likes` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_commentaire_articles` (`article_id`),
-  KEY `fk_commentaire_users` (`user_id`)
+  `contenu` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `article_id` int(10) UNSIGNED NOT NULL,
+  `likes` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `file_upload`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` text NOT NULL,
-  `prenom` text NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `mot_de_passe` varchar(30) NOT NULL,
-  `image_profil` longblob NOT NULL,
-  `date_creation` date NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `file_upload` (
+  `id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mot_de_passe` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image_profil` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_creation` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contraintes pour les tables déchargées
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nom`, `prenom`, `email`, `mot_de_passe`, `image_profil`, `date_creation`) VALUES
+(1, 'barry', 'mamadou', 'barrdqs@gmail.com', '$2y$10$PDpMOuQaDadnoQGV3fNY0.k38e/4uFNsrbmjx4hfWVMKUJckjeBAy', 'avatars/cards.png', '2021-07-31'),
+(2, 'barry2', 'elhadj', 'test@gmail.com', '$2y$10$o0LFKURBUE.jlCZFzTL8MemRNHWgXg9R93crnmJQhrAj4YsASZV7i', 'avatars/Capture.PNG', '2021-07-31'),
+(3, 'test', 'tester', 't@gmail.com', '$2y$10$2DMR6b9NcX.ZnJot0H5MweRO/mQrujtjRtDHPT/hfr1JAXW2x3tNa', 'avatars/JCVD.jpg', '2021-08-02');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `articles`
+-- Indexes for table `articles`
 --
 ALTER TABLE `articles`
-  ADD CONSTRAINT `fk_articles_categories` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  ADD CONSTRAINT `fk_articles_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_articles_categories` (`categorie_id`),
+  ADD KEY `fk_articles_users` (`user_id`);
 
 --
--- Contraintes pour la table `commentaires`
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `commentaires`
 --
 ALTER TABLE `commentaires`
-  ADD CONSTRAINT `fk_commentaire_articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  ADD CONSTRAINT `fk_commentaire_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_commentaire_articles` (`article_id`),
+  ADD KEY `fk_commentaire_users` (`user_id`);
+
+--
+-- Indexes for table `file_upload`
+--
+ALTER TABLE `file_upload`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `file_upload`
+--
+ALTER TABLE `file_upload`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `fk_articles_categories` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `fk_articles_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `fk_commentaire_articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`),
+  ADD CONSTRAINT `fk_commentaire_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
